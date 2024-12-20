c2f84b03fc5129664f58f0241562449e9e05d5c8
KurbyDoo <moomooman.kurbydoo@gmail.com>
2024-12-20 14:20:06 -0500

    fixed ui component bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,80 @@
-import { useState } from 'react'
-import Sim from './Sim'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// import {
+//   Carousel,
+//   CarouselContent,
+//   CarouselItem,
+//   CarouselNext,
+//   CarouselPrevious,
+// } from "@/components/ui/carousel"
+// import { Slider } from "@/components/ui/slider"
+// import { Switch } from "@/components/ui/switch"
+// import { Label } from "@/components/ui/label"
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//       <Carousel>
+//         <CarouselContent>
+//           <CarouselItem>...</CarouselItem>
+//           <CarouselItem>...</CarouselItem>
+//           <CarouselItem>...</CarouselItem>
+//         </CarouselContent>
+//         <CarouselPrevious />
+//         <CarouselNext />
+//       </Carousel>
+//       <Slider defaultValue={[33]} max={100} step={1} />
+//       <Switch />
+//       <Switch />
+//       <Switch />
+//       <div className="flex items-center space-x-2">
+//       <Switch id="airplane-mode" />
+//       <Label htmlFor="airplane-mode">Airplane Mode</Label>
+//     </div>
+//     </>
+//   )
+// }
+
+// export default App
+import { Button } from "@/components/ui/button"
+import { Switch } from "@/components/ui/switch"
+import { Label } from "@/components/ui/label"
 import './App.css'
+
 
 function App() {
 
   return (
-    <div className='flex justify-center items-center h-screen relative'>
-      {/* <div className='absolute z-10 text-xl font-bold'>under construction</div> */}
-      <Sim />
+    <div>
+        <Button variant="outline">Button</Button>
+        <div className="flex items-center space-x-2">
+          <Switch id="airplane-mode" />
+          <Label htmlFor="airplane-mode">Airplane Mode</Label>
+        </div>
     </div>
   )
 }
